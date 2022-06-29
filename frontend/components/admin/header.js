bd01cd67753c9637378759a093f558ef5a3447d6
tranmanhtuong123 <18130275@st.hcmuaf.edu.vn>
2022-06-29 08:05:37 +0700

    page admin

---
+++ frontend/components/admin/header.js
@@ -0,0 +1,33 @@
+import {
+    faAngleDown,
+    faBasketShopping,
+    faChevronDown,
+    faCircleCheck,
+    faClose,
+    faCog,
+    faEnvelope,
+    faHeart,
+    faHome,
+    faPhoneFlip,
+    faRightFromBracket,
+    faSearch,
+    faSignInAlt,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useSession } from "next-auth/client";
+import instance from "../../helpers/axiosConfig";
+import { useRouter } from 'next/router'
+import { changeLanguage } from "../../helpers/customFunction/changeLanguage";
+import { changeRoute } from "../../helpers/customFunction/changeRoute";
+import { useEffect, useState } from 'react';
+
+export default function HeaderAdmin() {
+
+    return (
+        <>
+            {/* Header */}
+
+            {/* Header /- */}
+        </>
+    )
+}
